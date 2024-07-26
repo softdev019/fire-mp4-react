a1d57f03ce77ce1068055ca43a0cc85a20723c7a
johnmiller0923 <softdev019@gmail.com>
2024-07-26 16:53:52 +1000

    organize code and fix sign in page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import SignInPage from './pages/signin';
 
